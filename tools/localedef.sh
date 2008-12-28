987f9a152844c1243587624c6df64d233fae6faf
hnhn <none@none>
2008-12-28 12:56:06 +0100

    6538731: use global CFLAGS, LOCALEDEF for locale generation

--- tools/localedef.sh
+++ tools/localedef.sh
@@ -22,43 +22,12 @@
 # Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
-# A shell script front-end for UTF-8 locale generation.
+# A shell script front-end for localedef invocation.
+# Accepts localedef's exit status 1 (warnings only).
 #
 
-if [ $# -lt 5 ] ; then
-	echo "Usage: $0 Charmap LocaleSrc MethodSrc Locale C_opt [ L_opt ]"
-	exit 1
-fi
+/usr/bin/localedef "$@"
 
-CHARMAP=$1
-LOCALEDEF=$2
-METHOD=$3
-LOCALE=$4
-COPT=$5
-LOPT=
-if [ $# -ge 6 ] ; then
-	LOPT=$6
-fi
-
-MACH=`uname -p`
-sparc_COPTFLAG="-xO3 -xregs=no%appl"
-i386_COPTFLAG="-xO3"
-
-COPTFLAG=`eval echo \\$${MACH}_COPTFLAG`
-CFLAGS="$COPTFLAG -K pic -D PIC -G -Xa -z text -z ignore -D_REENTRANT"
-
-if [ "X$COPT" != "X" ] ; then
-	LDF_OPT="-m lp64 "
-	CFLAGS="$CFLAGS -m64"
-fi
-
-if [ "X$LOPT" != "X" ] ; then
-	/usr/bin/localedef $LDF_OPT -c -v  -W cc,"$CFLAGS" \
-		-L "$LOPT" -x $METHOD -f $CHARMAP -i $LOCALEDEF $LOCALE
-else
-	/usr/bin/localedef $LDF_OPT -c -v  -W cc,"$CFLAGS" \
-		-x $METHOD -f $CHARMAP -i $LOCALEDEF $LOCALE
-fi
 RET_CODE=$?
 
 if [ $RET_CODE -ge 2 ] ; then
