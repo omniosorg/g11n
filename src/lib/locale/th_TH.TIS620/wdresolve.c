99ea4d76b73ba51f9ec8239451eb0653981e8bd0
hnhn <none@none>
2010-08-10 16:11:00 +0200

    6900712 [PSARC/2009/528] Remove obsolete short-form locales
    --HG--
    rename : src/lib/locale/th_TH/LCL_DEF => src/lib/locale/th_TH.TIS620/LCL_DEF
    rename : src/lib/locale/th_TH/Makefile => src/lib/locale/th_TH.TIS620/Makefile
    rename : src/lib/locale/th_TH/geo_map => src/lib/locale/th_TH.TIS620/geo_map
    rename : src/lib/locale/th_TH/geo_map.miniroot => src/lib/locale/th_TH.TIS620/geo_map.miniroot
    rename : src/lib/locale/th_TH/locale_description => src/lib/locale/th_TH.TIS620/locale_description
    rename : src/lib/locale/th_TH/locale_description.miniroot => src/lib/locale/th_TH.TIS620/locale_description.miniroot
    rename : src/lib/locale/th_TH/locale_map => src/lib/locale/th_TH.TIS620/locale_map
    rename : src/lib/locale/th_TH/locale_map.miniroot => src/lib/locale/th_TH.TIS620/locale_map.miniroot
    rename : src/lib/locale/th_TH/wdresolve.c => src/lib/locale/th_TH.TIS620/wdresolve.c
    rename : src/locale_src/base/th_TH.localedef => src/locale_src/base/th_TH.TIS620.localedef

--- src/lib/locale/th_TH.TIS620/wdresolve.c
+++ src/lib/locale/th_TH.TIS620/wdresolve.c
@@ -2,7 +2,7 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").  
+ * Common Development and Distribution License (the "License").
  * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at src/OPENSOLARIS.LICENSE
@@ -19,8 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
- * Use is subject to license terms.
+ * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
  */
 
 #ifdef	SCCS
